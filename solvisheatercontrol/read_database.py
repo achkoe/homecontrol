fedcd6263c182dde7d88f6c984e1dde87a5c9be3
achkoe <achkoe@online.de>
2026-03-03 08:36:11 +0100

    extended read_database.py

--- solvisheatercontrol/read_database.py
+++ solvisheatercontrol/read_database.py
@@ -10,6 +10,45 @@ def dict_factory(cursor, row):
     return {key: value for key, value in zip(fields, row)}
 
 
+def powerperday(cursor):
+    cursor.execute("SELECT * FROM heater ORDER BY time ASC")
+    item = cursor.fetchone()
+    if item is None:
+        return  # FIXME
+    previous_time = item["time"]
+    item["time"] = datetime.datetime.fromtimestamp(item["time"])
+    previous_day = item["time"].day
+    previous_month = item["time"].month
+    previous_year = item["time"].year
+    power = abs(item["power"])
+    work = 0.0
+        
+    while True:
+        item = cursor.fetchone()
+        if item is None:
+            break
+        current_time = item["time"]        
+        if 0:
+            print(f"{current_time:11.7f} | {previous_time:11.7f} | {(current_time - previous_time):6.1f} | {power}")
+        work += (current_time - previous_time) * power / (1000 * 60 * 60)
+        power = abs(item["power"])
+        previous_time = current_time
+        
+        item["time"] = datetime.datetime.fromtimestamp(item["time"])
+        day = item["time"].day
+        month = item["time"].month
+        year = item["time"].year
+        if day != previous_day or month != previous_month or year != previous_year:
+            previous_day = day
+            previous_month = month
+            previous_year = year
+            print(f"{previous_year}-{previous_month}-{previous_day} -> {work}")
+            work = 0.0
+        
+    print(f"{previous_year}-{previous_month}-{previous_day} -> {work} kWh")
+    
+    
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("-n", "--number", type=int, default=0, action="store", help="show last number of items, default %(default)i (means all)")
@@ -28,3 +67,7 @@ if __name__ == "__main__":
         args.number -= 1
         if args.number == 0:
             break
+
+    powerperday(cursor)
+    
+    connection.close()
