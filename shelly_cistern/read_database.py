b4b8cf759dc020b1fe0a92215f53a5d9a9afafd0
achkoe <achkoe@online.de>
2026-07-31 16:27:22 +0200

    saved work

--- shelly_cistern/read_database.py
+++ shelly_cistern/read_database.py
@@ -18,13 +18,50 @@ if __name__ == "__main__":
     connection.row_factory = dict_factory
     cursor = connection.cursor()
     
+    index = 0
+    
     cursor.execute("SELECT * FROM power ORDER BY time DESC")
     while True:
         item = cursor.fetchone()
         if item is None:
             break
+        item["i"] = index
+        index += 1
         item["time"] = datetime.datetime.fromtimestamp(item["time"])
-        print("|".join(f"{key}={item[key]}" for key in DBFIELDS))
+        print("|".join(f"{key}={item[key]}" for key in item))
         args.number -= 1
         if args.number == 0:
             break
+
+    rlist = []
+    elist = []
+    plimit = 50
+    index = 0
+    ison = False
+
+    cursor.execute("SELECT * FROM power ORDER BY time ASC")
+    while True:
+        item = cursor.fetchone()
+        if item is None:
+            break
+        elist.append(item)
+        if item["power"] > plimit and ison is False:
+            tstart = item["time"]
+            istart = index
+            ison = True
+        if item["power"] < plimit:
+            rlist.append((item["time"] - tstart, istart))   
+            ison = False
+        index += 1
+        
+    rlist.sort(key=lambda x: x[0])
+    
+    for index, item in enumerate(elist):
+        item["time"] = datetime.datetime.fromtimestamp(item["time"])
+        print("{0:3} | {1} | {2:5.1f}".format(index, item["time"], item["power"]))
+        
+    for index in range(3):
+        print("{1} -> {0}".format(*rlist[-index]))
+        
+    maxtime = rlist[-1]
+    print(f"Maximum on time is {maxtime[0]} seconds, {maxtime[0] / 60} minutes (at index {maxtime[1]})")
